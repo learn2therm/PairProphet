8314d92f86b7da802abe07c2abe2e47ea53b667c
Logan Roberts <loganwroberts98@gmail.com>
2023-03-08 12:47:35 -0800

    pipeline

--- Components/c5_pipeline.py
+++ Components/c5_pipeline.py
@@ -14,7 +14,7 @@ import c5_input_cleaning
 from c5_classification import train_model
 from c5_classification import test_model
 from c5_classification import plot_model
-from c5_classification import RF_wrapper
+from c5_classification import rf_wrapper
 
 from c5_input_cleaning import check_input_type
 from c5_input_cleaning import clean_input_columns
@@ -23,12 +23,14 @@ from c5_input_cleaning import check_input_NANs
 from c5_input_cleaning import verify_protein_pairs
 from c5_input_cleaning import input_cleaning_wrapper
 
+# autopep8 --in-place --aggressive --aggressive c5_pipeline.py
+
 df_original = pd.read_csv('learn2therm_sample_50k.csv')
 
 """
 This cleaning step is done because we want to use thermophyllic protein
 description as our target. I am eliminating categories without significant
-value counts in order to build a better model with our "dummy" target. 
+value counts in order to build a better model with our "dummy" target.
 This code will be deleted when we get our real target from Component 3.
 """
 
@@ -41,31 +43,30 @@ list_of_cats
 # process dataframe
 df = df_original[df_original.t_protein_desc.isin(list_of_cats)]
 
-df['protein_match'] = df['t_protein_desc'] == df['m_protein_desc']
+df['protein_match'] = df['t_protein_desc'].eq(df['m_protein_desc'])
+
+# will need to be adjusted when component 3 is finished
+columns_to_keep = [
+    'bit_score',
+    'local_gap_compressed_percent_id',
+    'scaled_local_query_percent_id',
+    'scaled_local_symmetric_percent_id',
+    'query_align_len',
+    'query_align_cov',
+    'subject_align_len',
+    'subject_align_cov',
+    'm_protein_len',
+    't_protein_len',
+    'protein_match']
 
 
-#columns we don't want from our own database
-df = df.drop(columns=['Unnamed: 0', 'thermo_index', 'm_protein_seq',
-                't_protein_seq', 'm_protein_desc', 't_protein_desc', 'query_align_cov_16s',
-                'subject_align_cov_16s', 'meso_index', 'meso_protein_int_index',
-                'local_gap_compressed_percent_id_16s', 'scaled_local_query_percent_id_16s',
-                'scaled_local_symmetric_percent_id_16s', 'bit_score_16s', 'm_ogt', 
-                't_ogt', 'taxa_pair_index', 'thermo_protein_int_index', 
-                'prot_pair_index', 'ogt_difference'])
-
-#will need to be adjusted when component 3 is finished
-columns_to_keep = ['bit_score','local_gap_compressed_percent_id','scaled_local_query_percent_id',
-                      'scaled_local_symmetric_percent_id','query_align_len', 'query_align_cov',
-                      'subject_align_len', 'subject_align_cov', 'm_protein_len', 't_protein_len', 't_protein_desc']
-
-
-#run dataframe through component five wrapper
+# run dataframe through component five wrapper
 def c5_wrapper(dataframe):
 
-    #clean input dataframe
+    # clean input dataframe
     cleaned = input_cleaning_wrapper(dataframe)
 
-    #run through model
-    model = RF_wrapper(cleaned)
+    # run through model
+    model = rf_wrapper(cleaned)
 
     return model
