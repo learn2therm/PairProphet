cd2b5a95b7fccfbbcb942a77b9c1ac99c7b9adbc
Logan Roberts <loganwroberts98@gmail.com>
2023-05-01 09:36:47 -0700

    feature generation notebook and scripts

--- FAFSA/train_val_featuregen.py
+++ FAFSA/train_val_featuregen.py
@@ -111,43 +111,109 @@ def create_new_dataframe(dataframe, output_file, descriptors=[]):
 Functions below only necessary if input is fasta file.
 """
 
-# def remove_fasta_description(filename:str):
 
-#     """
-#     Removes description from fasta file so that iProtein can read the input.
-#     """
+def remove_fasta_description(filename: str):
+    """
+    Removes description from fasta file so that iProtein can read the input.
+    """
 
-#     #assign unwanted string to object
-#     string_to_remove = "<unknown description>"
+    # assign unwanted string to object
+    string_to_remove = "<unknown description>"
 
-#     #open file
-#     with open(filename, "r") as file:
-#         content = file.read()
+    # open file
+    with open(filename, "r") as file:
+        content = file.read()
 
-#     # Remove the string
-#     new_content = content.replace(string_to_remove, "")
+    # Remove the string
+    new_content = content.replace(string_to_remove, "")
 
-#     #overwrite file without string
-#     with open(filename, "w") as file:
-#         seq = file.write(new_content)
+    # overwrite file without string
+    with open(filename, "w") as file:
+        seq = file.write(new_content)
 
-#     return seq
-
-# def fasta_to_descriptors(fasta:str, descriptors=[]):
-
-#     """This function is for generating descriptors from a fasta file.
-#     Not necessary if sequences come from dataframe.
-
-#     Parameters
-#     ----------
-#     Fasta file with protein sequences.
-
-#     Returns
-#     -------
+    return seq
 
 
-#     #remove description from fasta file
-#     remove_fasta_description(fasta)
+"""
+Function below is necessary for inputs with
+two protein sequences.
+"""
 
-#     #return protein descriptors
-#     return get_protein_descriptors(fasta, descriptors=descriptors)
+def get_fasta_from_dataframe(dataframe, output_file_a, output_file_b):
+    """
+    adjust this to write function with BioPython
+    separate functions for each of the input sequences
+    in training, seq_a = meso and seq_b = thermo
+    """
+    
+    
+    #meso sequence to fasta
+    with open(output_file_a, 'w') as f:
+        for _, row in df.iterrows():
+            f.write('>{}\n{}\n'.format((row['prot_pair_index']), row['m_protein_seq']))
+    
+    #thermo sequence to fasta
+    with open(output_file_b, 'w') as f:
+        for _, row in df.iterrows():
+            f.write('>{}\n{}\n'.format((row['prot_pair_index']), (row['t_protein_seq'])))
+   
+    #return output files
+    return [output_file_a, output_file_b]
+
+def create_new_dataframe(dataframe, output_files, descriptors=[]):
+    """
+    Creates new dataframe with descriptors added.
+
+    Parameters
+    ----------
+    Pandas dataframe, list of descriptors as strings, output file name.
+
+    Returns
+    -------
+    Dataframe including vector(s) of descriptors
+    """
+
+    fasta_files = get_fasta_from_dataframe(
+        dataframe, output_files[0], output_files[1])
+
+    def compute_descriptor_difference(fasta_files, descriptors=[]):
+        """
+        Generates dictionary of descriptors for each of the two input sequences.
+        Computes the difference between each instance of a descriptor.
+
+        Parameters
+        ----------
+        List of two fasta files (str) and list of descriptors (str).
+
+        Returns
+        -------
+        Dictionary with difference between descriptors for each of the
+        input sequences.
+        """
+        desc_a = get_protein_descriptors(fasta_files[0], descriptors)
+        desc_b = get_protein_descriptors(fasta_files[1], descriptors)
+
+        feature_dict = {}
+
+        for key in desc_a:
+            feature_dict[key] = desc_a[key] - desc_b[key]
+
+        return feature_dict
+
+    feature_dict = compute_descriptor_difference(fasta_files, descriptors)
+
+    df = dataframe.reset_index()
+
+    for desc in descriptors:
+
+        feature_dict[desc].index = feature_dict[desc].index.astype(int)
+        features = feature_dict[desc].reset_index()
+
+        df = pd.merge(
+            df,
+            features,
+            how='outer',
+            left_index=True,
+            right_index=True)
+
+    return df
