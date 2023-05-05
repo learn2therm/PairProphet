be71227c718854594c87d7927356d990a3bff384
Chau Vuong <vchau085@gmail.com>
2023-05-04 17:36:10 -0700

    wrapper_function

---
+++ FAFSA/main_pseudo.py
@@ -0,0 +1,39 @@
+"""
+Main script wrapper.
+This is pseudocode for our parent script.
+Directory: ValidProt.
+Input: 
+Output: 
+
+Note: We have two wrapping functions, but 
+these can be developed as individual scripts.
+Runtime: 
+"""
+
+def model_dev(sequences):
+
+    #Ryans Component
+
+    #Humood/Amin Component
+
+    #Chau Component
+    
+    #Logan Component
+    """
+    Input: Dataframe with BLAST metrics/sequences from Ryan (pandas dataframe) 
+    + Boolean generated from Humood (.csv)
+
+    Output: Pandas dataframe with 4 columns (seq1, seq2, protein_match, Jaccard Score)
+
+    Params: Base environment + iFeatureOmega dependencies 
+    """
+    model_output = rf_wrapper(blast_seq, boolean1, boolean2)
+
+    return model_output
+
+
+def main():
+
+    """
+    Function for user to interact with.
+    """
