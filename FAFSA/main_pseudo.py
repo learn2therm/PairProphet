dae7230ac0e9c28d1c22b43866254c63e6a93347
Chau Vuong <vchau085@gmail.com>
2023-05-05 11:49:27 -0700

    updated_wrapper

--- FAFSA/main_pseudo.py
+++ FAFSA/main_pseudo.py
@@ -12,20 +12,35 @@ Runtime:
 def model_dev(sequences):
 
     #Ryans Component
+    """
+    Input: learn2thermDB (queuing format)
+    Output: SQL db after filtering, and then each component can retrieve the data as needed.
+    Params: Base environment + DuckDB
+    """
 
     #Humood/Amin Component
-
+        ##Humood:
+    """
+    Input:
+    Output:
+    Params:
+    """
+        ##Amin:
+    """
+    Input: SQL db from Ryan, then retrieve only 1000 pairs
+    Output: SQL db with appended Boolean from running HMMR remotely
+    Params: Base environment + HMMR API
+    """
     #Chau Component
     """
-    Input: Dataframe with PDB IDs and Uniprot IDs (sequences are not required for this component)
-    Output: Boolean on whether the structures are similar
+    Input: SQL db from Ryan, retrieve only PDB IDs and Uniprot IDs (sequences are not required for this component)
+    Output: SQL db appended with Boolean on whether the structures are similar
     Params: Base environment + BioPython + FATCAT
     """
     #Logan Component
     """
-    Input: Dataframe with BLAST metrics/sequences from Ryan (pandas dataframe) 
-    + Boolean generated from Humood (.csv)
-    Output: Pandas dataframe with 4 columns (seq1, seq2, protein_match, Jaccard Score)
+    Input: SQL db from Humood or Amin (with HMMR Boolean + prot_pair_ind + Jaccard score appended)
+    Output: csv files with 6 columns (seq1, seq2, protein_match (Humood + Amin), protein_match(ML), Jaccard Score)
     Params: Base environment + iFeatureOmega dependencies 
     """
     model_output = rf_wrapper(blast_seq, boolean1, boolean2)
@@ -37,4 +52,17 @@ def main():
 
     """
     Function for user to interact with.
+    """
+    #Ryans Component
+    """
+    Input: csv file (list of sequences) + pairing and PDBids and UniprotIDs are optional
+    Output:chunked csv for HMMR and structure components.
+        Note: csv with specific parameters will be generated for specific component.
+    Params:
+    """
+    #Logan Component
+    """
+    Input: SQL db from Humood or Amin + Chau (with HMMR Boolean + prot_pair_ind + Jaccard score appended + FATCAT)
+    Output: csv files with 6 columns (seq1, seq2, protein_match (Humood + Amin), protein_match (Chau) protein_match(ML), Jaccard Score)
+    Params: Base environment + iFeatureOmega dependencies 
     """
