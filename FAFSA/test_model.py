786afcd1c4709b8777c01760a098378b4117955d
Logan Roberts <loganwroberts98@gmail.com>
2023-05-19 15:50:39 -0700

    model testing script for user

--- FAFSA/test_model.py
+++ FAFSA/test_model.py
@@ -15,6 +15,13 @@ from sklearn.ensemble import BaggingClassifier, \
     GradientBoostingClassifier, \
     ExtraTreesClassifier
 
+from sklearn.metrics import accuracy_score, \
+    confusion_matrix, \
+    roc_auc_score,\
+    average_precision_score,\
+    auc,\
+    roc_curve, f1_score, recall_score, matthews_corrcoef, auc
+
 # create dictionary of models
 names = ['LR', 'KNN', 'DT', 'NB', 'RF', 'Bagging', 'AB', 'GB', 'SVM']
 
@@ -50,6 +57,16 @@ classifiers = [
     #     SVC(),
 ]
 
+F = open('evaluationResults.txt', 'w')
+
+F.write('Evaluation Scale:'+'\n')
+F.write('0.0% <=Accuracy<= 100.0%'+'\n')
+F.write('0.0 <=AUC<= 1.0'+'\n') #area under curve
+F.write('0.0 <=auPR<= 1.0'+'\n')  # average_Precision
+F.write('0.0 <=F1_Score<= 1.0'+'\n')
+F.write('-1.0 <=MCC<= 1.0'+'\n')
+F.write('_______________________________________'+'\n')
+
 
 def test_model(args, test_X, test_y):
     """
@@ -68,20 +85,63 @@ def test_model(args, test_X, test_y):
 
     model = args.classifier
 
+    accuracy = []
+    avg_precision = []
+    F1_Score = []
+    AUC = []
+    MCC = []
+    Recall = []
+
     # test input arguments
     assert "sklearn" in str(type(model))
     assert "numpy.ndarray" in str(type(test_X))
     assert "numpy.ndarray" in str(type(test_y))
 
+    # scale data
+    scaler = sklearn.preprocessing.StandardScaler()
+    test_X = scaler.fit_transform(test_X)
+
+    #vector of predictions
     preds = model.predict(test_X)
 
-    # not printed during model validation step
+    # calculate precision score
     precision_score = sklearn.metrics.precision_score(test_y, preds)
+
+    # Calculate ROC Curve and Area the Curve
+    proba_y = model.predict_proba(test_X)[:,1]
+    FPR, TPR, _ = roc_curve(test_y, proba_y, pos_label=1)
+    roc_auc = auc(FPR, TPR)
+    
+    #calculate scoring metrics
+    #include option to return these scores
+    accuracy.append(accuracy_score(y_pred=preds, y_true=test_y))
+    avg_precision.append(average_precision_score(y_true=test_y, y_score=proba_y, pos_label=1))
+    F1_Score.append(f1_score(y_true=test_y, y_pred=preds, pos_label=1))
+    MCC.append(matthews_corrcoef(y_true=test_y, y_pred=preds))
+    Recall.append(recall_score(y_true=test_y, y_pred=preds, pos_label=1))
+    AUC.append(roc_auc)
+
+    confusion_matrix = sklearn.metrics.confusion_matrix(y_pred=preds, y_true=test_y)
+    sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix).plot()
+
+    accuracy = [_*100.0 for _ in accuracy]
+    Results[name + ' Accuracy, F1 Score'] = [accuracy, F1_Score]
+    
+    F.write('Classifier: {}\n'.format(name))
+    F.write('Accuracy: {0:.4f}%\n'.format(np.mean(accuracy)))
+    F.write('AUC: {0:.4f}\n'.format( np.mean(AUC)))
+    F.write('auPR: {0:.4f}\n'.format(np.mean(avg_precision))) # average_Precision
+    F.write('F1_Score: {0:.4f}\n'.format(np.mean(F1_Score)))
+    F.write('MCC: {0:.4f}\n'.format(np.mean(MCC)))
+
+#         TN, FP, FN, TP = CM.ravel()
+    F.write('Recall: {0:.4f}\n'.format( np.mean(Recall)) )
+    F.write('_______________________________________'+'\n')
 
     return preds, precision_score
 
+
 if __name__ == '__main__':
-     # print('Please, enter number of cross validation:')
     import argparse
     p = argparse.ArgumentParser(description='Choose your classification model!')
 
