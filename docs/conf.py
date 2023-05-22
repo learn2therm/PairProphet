2e096c772e9a46cf1314ebc64b7226c973482ddd
Logan Roberts <loganwroberts98@gmail.com>
2023-05-21 19:57:38 -0700

    RTD stuff

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ extensions = [
     'sphinx.ext.autosectionlabel'
 ]
 
-autodoc_mock_imports = ['pandas', 'numpy,', 'matplotlib', 'sklearn']
+autodoc_mock_imports = ['pandas', 'numpy', 'matplotlib', 'sklearn', 'joblib', 'iFeatureOmegaCLI']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
