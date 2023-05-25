4765b567e6d78ea7ade588a0160c30d19f1f45cc
ryfran <ryfran@uw.edu>
2023-05-25 14:52:50 -0700

    Working user_blast component. Cleaned up RMF files in FAFSA.

--- FAFSA/user_blast.py
+++ FAFSA/user_blast.py
@@ -1,28 +1,50 @@
+from Bio import Align
+from Bio.Align import substitution_matrices
 import re
-import pandas as pd
 import numpy as np
+import pandas as pd
+import duckdb
 
-from Bio.Blast.Applications import NcbimakeblastdbCommandline, NcbiblastpCommandline
-from Bio.Blast import NCBIXML
+def make_blast_df(df, mode = 'local'):
+    """
+    This function generates pairwise alignment scores for a set of protein sequences.
+    -------------
+    Parameters:
+    -------------
+    df: pandas.core.DataFrame
+        A 2-column DataFrame containing the query and subject sequences for alignment.
+    mode: str
+        'local' or 'global'. Default: 'local'.
+    -------------
+    Raises:
+    -------------
+    Exception:
+        
+    -------------
+    Returns:
+    -------------
+    blast_df: pandas.core.DataFrame
+        A dataframe with the input sequence pairs, associated id values, and alignment scores.
+    """   
+    # Rename input data columns for compatibility
+    original_cols = df.columns
+    df.rename(columns = {original_cols[0]: 'query', original_cols[1]: 'subject'}, inplace = True)
 
-def make_blast_df(data):
-
-    original_cols = data.columns
-
-    data.rename(columns = {original_cols[0]: 'query', original_cols[1]: 'subject'}, inplace = True)
-
-    n_unique_1 = np.unique(data['query'])
-    n_unique_2 = np.unique(data['subject'])
+    # Generate protein ids for each unique query and subject sequence
+    n_unique_1 = np.unique(df['query'])
+    n_unique_2 = np.unique(df['subject'])
 
     qid_dict = dict(zip(n_unique_1, range(len(n_unique_1))))
     sid_dict = dict(zip(n_unique_2, range(len(n_unique_2))))
 
-    data['qid'] = [qid_dict[i] for i in data.iloc[:,0]]
-    data['sid'] = [sid_dict[i] for i in data.iloc[:,1]]
+    df['qid'] = [qid_dict[i] for i in df.iloc[:,0]]
+    df['sid'] = [sid_dict[i] for i in df.iloc[:,1]]
 
-    data['pid'] = data.index
+    # Use unique row index as a pair id
+    df['pid'] = df.index
 
-    m = [
+    # Metrics to be calculated
+    metrics = [
     'local_gap_compressed_percent_id',
     'scaled_local_query_percent_id',
     'scaled_local_symmetric_percent_id',
@@ -33,141 +55,119 @@ def make_blast_df(data):
     'bit_score'
     ]
 
-    final = pd.DataFrame()
+    data_dict = df.to_dict('records')
+    final = pd.DataFrame(columns = metrics)
+      
+    # Initialize PairWiseAligner with required parameters for model
+    aligner = Align.PairwiseAligner()
+    aligner.substitution_matrix = substitution_matrices.load("BLOSUM62")
+    aligner.open_gap_score = -11
+    aligner.extend_gap_score = -1
+    aligner.mode = mode
 
-    db = open('db.fasta','w')
+    alignments = []
+    final_data = []
+    
+    # Iterate through all pairs and calculate best alignment and metrics
+    for row in data_dict:
+        query = row['query']
+        subject = row['subject']
 
-    for row in data.iterrows():
+        alignment = aligner.align(subject, query)
+        best_alignment = max(alignment, key=lambda x: x.score)
 
-        db = open('db.fasta','w')
-        db.write(f">{row[1]['sid']}\n{row[1]['subject']}\n")
-        db.close()
+        seq1_aligned = best_alignment[0]
+        seq2_aligned = best_alignment[1]
+        
+        # Coverage
+        subject_cov = sum(c != '-' for c in seq1_aligned) / len(seq1_aligned)
+        query_cov = sum(c != '-' for c in seq2_aligned) / len(seq2_aligned)
+        
+        # Sequence length
+        l_subject = len(query)
+        l_query = len(subject)
+        
+        # Percent ids
+        n_matches, n_gaps, n_columns, n_comp_gaps = get_matches_gaps(seq2_aligned, seq1_aligned)
+        gap_comp_pct_id = gap_compressed_percent_id(n_matches, n_gaps, n_columns, n_comp_gaps)
+        scaled_local_symmetric_percent_id = 2 * n_matches / (l_subject + l_query)
+        scaled_local_query_percent_id = n_matches / l_query
+        
+        # Collect calculated metrics into a list for addition to final_data
+        new_row = [gap_comp_pct_id, scaled_local_query_percent_id, scaled_local_symmetric_percent_id, l_query,
+                   query_cov, l_subject, subject_cov, best_alignment.score, row['qid'], row['sid']]
+        final_data.append(new_row)
 
-        query = open('query.fasta','w')
-        query.write(f">{row[1]['qid']}\n{row[1]['query']}\n")
-        query.close()
-
-        cmd = NcbimakeblastdbCommandline(dbtype='prot', input_file='db.fasta', parse_seqids=True)()
-
-        NcbiblastpCommandline(query='query.fasta', db='db.fasta', outfmt=5, out='test_out', word_size=3,
-                             gapopen = 11, gapextend = 1)()
-
-        result_handle = open('test_out')
-
-        blast_records = NCBIXML.parse(result_handle)
-
-        test = BlastMet(blast_records)
-
-        outdf = test.compute_metric('ids')[['query_id','subject_id']]
-
-        for metric in m:
-            outdf[metric] = test.compute_metric(metric)[metric]
-
-        final = pd.concat([final, outdf], ignore_index = True)
-
-    final = final.astype({"query_id": int, "subject_id": int})
-
-    blast_df = data.merge(final, left_on = ['qid','sid'], right_on = ['query_id','subject_id'])
-
+    # Construct temporary dataframe from collected metrics
+    columns = metrics + ['query_id', 'subject_id']
+    final_df = pd.DataFrame(final_data, columns=columns)
+    
+    # Merge final_df with sequences and ids from input df
+    blast_df = df.merge(final_df, left_on=['qid', 'sid'], right_on=['query_id', 'subject_id'])
+    
     return blast_df
-
-
-class BlastMet:
-    """Handles computation of metrics for each alignment in a blast record.
-
-    The HSP with the largest average sequence coverage is used for local metrics.
-
-    Parameters
-    ----------
-    blast_record : the record containing all hits for a query
+ 
+    
+def get_matches_gaps(query, subject):
     """
-    def __init__(self, blast_record):
-        recs = []
-        for rec in blast_record:
-            recs.append(rec)           # This is clunky
-        self.record = recs[0]
-        self.qid = self.record.query.split(' ')[0]
+    Parses sequence alignment text to calculate the number of matches, gaps, compressed gaps, and total columns.
+    -------------
+    Parameters:
+    -------------
+    query: str
+        Query aligned sequence.
+    subject: str
+        Subject aligned sequence.
+    -------------
+    Raises:
+    -------------
+    Exception:
+        
+    -------------
+    Returns:
+    -------------
+    n_matches: int
+        Number of matching amino acids in the sequence alignment.
+    n_gaps: int
+        Total number of gaps across both aligned sequences.
+    n_columns: int
+        Length of the aligned query sequence.
+    n_comp_gaps: int
+        Number of compressed gaps.
+    """
+    n_columns = len(query)
+    n_gaps = sum(i == '-' for i in query) + sum(i == '-' for i in subject)
+    n_matches = sum(i == j and i != '-' for i,j in zip(query, subject))
+    pattern = r'(-{1,})'
+    
+    instances = re.findall(pattern, query) + re.findall(pattern, subject)
+    n_comp_gaps = len(instances)
+    
+    return n_matches, n_gaps, n_columns, n_comp_gaps
 
-    def id_hsp_best_cov(self, alignment):
-        """Determine HSP with the most average coverage of both sequences.
 
-        Returns
-        -------
-        Index of HSP with max average seq coverage
-        Max average coverage
-        """
-        scores = []
-
-        for hsp in alignment.hsps:
-            scores.append(
-                ((hsp.query_end +1 - hsp.query_start)/self.record.query_length +
-                 (hsp.sbjct_end +1 - hsp.sbjct_start)/alignment.length)/2)
-        return np.argmax(scores), max(scores)
-
-    @staticmethod
-    def raw_gap_compressed_percent_id(n_matches, n_gaps, n_columns, n_compressed_gaps):
-        """Percent matches in sequence, including but compressing gaps.
-
-        Parameters
-        ----------
-        n_matches : int, number of matches in match columns
-        n_gaps : number of gaps in match columns
-        n_columns : total number of alignment match columns
-        n_compressed_gaps : number of compressed gaps in match columns
-        """
-        return n_matches / (n_columns - n_gaps + n_compressed_gaps)
-
-    def compute_metric(self,metric_name: str):
-        """Compute the metric with specified name for each alignment"""
-        if not hasattr(self, metric_name):
-            raise ValueError(f"No metric found with name : {metric_name}")
-        else:
-            metric = getattr(self, metric_name)
-
-        outputs = []
-        for alignment in self.record.alignments:
-            hsp_id, _ = self.id_hsp_best_cov(alignment)
-            hsp = alignment.hsps[hsp_id]
-            outputs.append((self.qid, alignment.hit_id.split('|')[-1], metric(alignment, hsp)))
-        return pd.DataFrame(data=outputs, columns=['query_id', 'subject_id', metric_name])
-
-    def ids(self, alignment, hsp):
-        return 0
-
-    def subject_align_cov(self, alignment, hsp):
-        """Fraction of AA on query string taken up by alignment"""
-        return (hsp.sbjct_end +1 - hsp.sbjct_start)/alignment.length
-
-    def subject_align_len(self, alignment, hsp):
-        """Length of AA on query string taken up by alignment"""
-        return int(hsp.sbjct_end +1 - hsp.sbjct_start)
-
-    def query_align_cov(self, alignment, hsp):
-        """Fraction of AA on query string taken up by alignment"""
-        return (hsp.query_end +1 - hsp.query_start)/self.record.query_length
-
-    def query_align_len(self, alignment, hsp):
-        """Length of AA on query string taken up by alignment"""
-        return int(hsp.query_end +1 - hsp.query_start)
-
-    def bit_score(self, alignment, hsp):
-        return hsp.score
-
-    def local_gap_compressed_percent_id(self, alignment, hsp):
-        """Percent matches in match sequence, including but compressing gaps.
-
-        The largest local HSP score is used
-        """
-        n_matches = hsp.identities
-        n_gaps = hsp.gaps
-        n_columns = len(hsp.query)
-        n_compressed_gaps = len(re.findall('-+', hsp.query))+len(re.findall('-+', hsp.sbjct))
-        return self.raw_gap_compressed_percent_id(n_matches, n_gaps, n_columns, n_compressed_gaps)
-
-    def scaled_local_query_percent_id(self, alignment, hsp):
-        """Percent matches in query sequence based on best HSP."""
-        return hsp.identities/self.record.query_length
-
-    def scaled_local_symmetric_percent_id(self, alignment, hsp):
-        """Percent matches compared to average seq length of query and subject based on best HSP"""
-        return 2*hsp.identities/(self.record.query_length + alignment.length)
+def gap_compressed_percent_id(n_matches, n_gaps, n_columns, n_comp_gaps):
+    """
+    Calculates the percent id with compressed gaps.
+    -------------
+    Parameters:
+    -------------
+    n_matches: int
+        Number of matches in match columns
+    n_gaps: int
+        Number of gaps in match columns
+    n_columns: int
+        Total number of alignment match columns
+    n_compressed_gaps: int
+        Number of compressed gaps in match columns
+    -------------
+    Raises:
+    -------------
+    Exception:
+        
+    -------------
+    Returns:
+    -------------
+    n_matches / (n_columns - n_gaps + n_comp_gaps)
+    """
+    return n_matches / (n_columns - n_gaps + n_comp_gaps)
