3e9cc2b55d309f44ea421cbf26742da158d935f8
Chau Vuong <vchau085@gmail.com>
2023-06-04 22:31:09 -0700

    structures with docstrings

--- pairpro/structures.py
+++ pairpro/structures.py
@@ -1,3 +1,7 @@
+"""
+This module takes in a pandas dataframe containing Uniprot IDs and PDB IDs, download the pdb files and run FATCAT for structural alignment purpose.
+Returns a Boolean for structure similarity (O is False pair and 1 is True pair) or NaN if no structures were found for proteins.
+"""
 from Bio.PDB import PDBList
 import os
 import pandas as pd
@@ -13,9 +17,18 @@ import numpy as np
 import csv
 from joblib import Parallel, delayed
 
-import httpx
-
 async def download_aff(session, url, filename):
+    """
+    Downloads a file asynchronously using an HTTP session.
+
+    Args:
+        session (httpx.AsyncClient): An HTTP session for making requests.
+        url (str): The URL of the file to download.
+        filename (str): The name of the file to save.
+
+    Returns:
+        bool: True if the file is successfully downloaded, False otherwise.
+    """
     try:
         response = await session.get(url)
         if response.status_code == 200:
@@ -31,6 +44,17 @@ async def download_aff(session, url, filename):
         return False
 
 async def download_af(row, u_column, pdb_dir):
+    """
+    Downloads AlphaFold files for a given row asynchronously.
+
+    Args:
+        row (pd.Series): The row containing the data for the download.
+        u_column (str): The column name for the UniProt ID.
+        pdb_dir (str): The directory to save the downloaded files.
+
+    Returns:
+        bool: True if the download is successful, False otherwise.
+    """
     uniprot_id = getattr(row, u_column)
     url = f'https://alphafold.ebi.ac.uk/files/AF-{uniprot_id}-F1-model_v4.pdb'
     filename = f'{pdb_dir}/{uniprot_id}.pdb'
@@ -40,6 +64,18 @@ async def download_af(row, u_column, pdb_dir):
         return success
 
 def run_download_af_all(df, pdb_column, u_column, pdb_dir):
+    """
+    Runs the asynchronous download of AlphaFold files for all rows in a DataFrame.
+
+    Args:
+        df (pd.DataFrame): The DataFrame containing the data for the downloads.
+        pdb_column (str): The column name for the PDB ID.
+        u_column (str): The column name for the UniProt ID.
+        pdb_dir (str): The directory to save the downloaded files.
+    
+    Returns:
+        files: pdb files containing structural information.
+    """
     nest_asyncio.apply()
 
     async def download_af_all():
@@ -62,6 +98,16 @@ def run_download_af_all(df, pdb_column, u_column, pdb_dir):
     asyncio.run(download_af_all())
 
 def download_pdb(df, pdb_column, pdb_dir):
+    """
+    Downloads PDB files for the given DataFrame based on PDB IDs.
+
+    Args:
+        df (pd.DataFrame): The DataFrame containing the PDB IDs.
+        pdb_column (str): The column name for the PDB ID.
+        pdb_dir (str): The directory to save the downloaded files.
+    
+    Returns: pdb files containing structural information.
+    """
     pdbl = PDBList()
     for i, row in df.iterrows():
         pdb_id = row[pdb_column]
@@ -74,6 +120,17 @@ def download_pdb(df, pdb_column, pdb_dir):
                 pass
 
 def download_structure(df, pdb_column, u_column, pdb_dir):
+    """
+    Downloads structure files for a DataFrame using AlphaFold and PDB.
+
+    Args:
+        df (pd.DataFrame): The DataFrame containing the data for the downloads.
+        pdb_column (str): The column name for the PDB ID.
+        u_column (str): The column name for the UniProt ID.
+        pdb_dir (str): The directory to save the downloaded files.
+
+    Returns: pdb files containing structural information.
+    """
     start_time = time.time()  # Start measuring time
     if not os.path.exists(pdb_dir):
         os.makedirs(pdb_dir)
@@ -85,6 +142,18 @@ def download_structure(df, pdb_column, u_column, pdb_dir):
     pass
 
 def compare_fatcat(p1_file, p2_file, pdb_dir, pair_id):
+    """
+    Compares two protein structures using FATCAT.
+
+    Args:
+        p1_file (str): The path to the first protein structure file.
+        p2_file (str): The path to the second protein structure file.
+        pdb_dir (str): The directory containing the structure files.
+        pair_id (str): The ID of the protein pair.
+
+    Returns:
+        dict: A dictionary containing the pair ID and the p-value.
+    """
     # Set the FATCAT command and its arguments
     cmd = ['FATCAT', '-p1', p1_file, '-p2', p2_file, '-i', pdb_dir, '-q']
 
@@ -105,6 +174,16 @@ def compare_fatcat(p1_file, p2_file, pdb_dir, pair_id):
         return {'pair_id': pair_id, 'p_value': 0}
 
 def process_row(row, pdb_dir):
+    """
+    Processes a row of a DataFrame to compare protein structures using FATCAT.
+
+    Args:
+        row (pd.Series): The row containing the data for the comparison.
+        pdb_dir (str): The directory containing the structure files.
+
+    Returns:
+        dict: A dictionary containing the pair ID and the p-value.
+    """
     if not pd.isna(row['meso_pdb']):
         p1 = row['meso_pdb']
     else:
@@ -125,6 +204,17 @@ def process_row(row, pdb_dir):
     return compare_fatcat(p1_file, p2_file, pdb_dir, row['pair_id'])
 
 def run_fatcat_dict_job(df, pdb_dir, file):
+    """
+    Runs the FATCAT comparison job on a DataFrame and saves the results to a file.
+
+    Args:
+        df (pd.DataFrame): The DataFrame containing the data for the comparison.
+        pdb_dir (str): The directory containing the structure files.
+        file (str): The path to the output file.
+
+    Returns:
+        csv: a csv file containing the pair ID and the p-value.
+    """
     p_values = []  # List to store the extracted p-values
 
     # Parallelize the execution of the function using joblib
@@ -135,12 +225,13 @@ def run_fatcat_dict_job(df, pdb_dir, file):
         writer.writerows(p_values)
     return file
 
-if __name__ == '__main__':
-    dff = pd.read_csv('../data/chau_test.csv')
-    df = dff.sample(3)
-    # Download the structures
-    download_structure(df, 'meso_pdb', 'meso_pid', 'af')
-    download_structure(df, 'thermo_pdb', 'thermo_pid', 'af')
+# Uncomment the following lines to run the code as a script
+# if __name__ == '__main__':
+#     dff = pd.read_csv('../data/chau_test.csv')
+#     df = dff.sample(3)
+#     # Download the structures
+#     download_structure(df, 'meso_pdb', 'meso_pid', 'af')
+#     download_structure(df, 'thermo_pdb', 'thermo_pid', 'af')
 
-    # Compare the structures and extract the p-values
-    p_values = run_fatcat_dict_job(df, 'af', 'structure_alig.csv')
+#     # Compare the structures and extract the p-values
+#     p_values = run_fatcat_dict_job(df, 'af', 'structure_alig.csv')
